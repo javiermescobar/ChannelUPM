384b8a3c29c8076c9f80e66053abe64be197ac09
Javier Martín Escobar <javier.mescobar@alumnos.upm.es>
2023-02-06 11:01:25 +0100

    Create base activity and main activity, also appcompat and core dependencies updated

---
+++ app/src/main/java/com/javier/channelupm/activities/BaseActivity.kt
@@ -0,0 +1,14 @@
+package com.javier.channelupm.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+abstract class BaseActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(getLayoutResource())
+    }
+
+    protected abstract fun getLayoutResource(): Int
+}
