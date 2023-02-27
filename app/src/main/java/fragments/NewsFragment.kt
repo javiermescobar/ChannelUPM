6b1618776d6687cc7da278da200da967958dd677
Javier Martín Escobar <javier.mescobar@alumnos.upm.es>
2023-02-27 10:00:08 +0100

    Add view binding to news fragment

--- app/src/main/java/fragments/NewsFragment.kt
+++ app/src/main/java/fragments/NewsFragment.kt
@@ -6,13 +6,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.javier.channelupm.R
+import com.javier.channelupm.databinding.FragmentNewsBinding
 
 class NewsFragment : Fragment() {
+
+    private lateinit var binding: FragmentNewsBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_news, container, false)
+        binding = FragmentNewsBinding.inflate(layoutInflater)
+        return binding.root
     }
 }
